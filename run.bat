3b81f1592942d002a002621b325a384cb7141476
Harshit N Vasoya <Harshit.Vasoya@securemeters.com>
2025-08-01 11:34:39 +0530

    Initial commit :MobSF source code

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ if [%1]==[] goto usage
 SET conf=%1
 goto :run
 :usage
-SET conf="0.0.0.0:8000 [::]:8000"
+SET conf="127.0.0.1:8000 [::]:8000"
 :run
 echo Running MobSF on %conf%
 poetry run waitress-serve --listen=%conf% --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
